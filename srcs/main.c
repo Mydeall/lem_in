e90ff189efdbec28466db5e8012e3bb6c3eb80ca
Corentin CEPRE <ccepre@e2r9p12.42.fr>
2019-04-04 14:55:53 +0200

    fonctions free

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,54 @@
 /*   By: rkirszba <rkirszba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 18:52:43 by rkirszba          #+#    #+#             */
-/*   Updated: 2019/03/28 17:30:35 by rkirszba         ###   ########.fr       */
+/*   Updated: 2019/04/04 12:12:20 by ccepre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+void	free_room(t_room *room)
+{
+	t_link	*current_link;
+	t_link	*tmp;
+
+	while (room->params)
+		param_next(&(room->params));
+	current_link = room->links;
+	while (current_link)
+	{
+		tmp = current_link;
+		current_link = current_link->next;
+		free(tmp);
+	}
+	free(room->name);
+	free(room);
+}
+	
+
+void	free_map(t_map *map)
+{
+	int		i;
+	t_room	*current_room;	
+	t_room	*tmp;
+
+	i = -1;
+	while (++i < HASH_SIZE)
+	{
+		current_room = NULL;
+		if (map->hash_tab[i])
+			current_room = map->hash_tab[i];
+		while (current_room)
+		{
+			tmp = current_room;		
+			current_room = current_room->next;
+			free_room(tmp);
+		}
+	}
+	free(map->hash_tab);
+	free(map);
+}
+
 
 int				main(void)
 {
@@ -24,12 +67,16 @@ int				main(void)
 	if (!(tab_parser = initialize_tab_parser(tab_parser))
 			|| !(map = initialize_map(map)))
 	{
-//		free_all
+		if (tab_parser)
+			free(tab_parser);
+		if (map)
+			free_map(map);
+//		free alll
 		return (1);
 	}
 	parser(map, tab_parser, line);
-//	print_map(map);
-//	edmonds_karp(map);
+	free(tab_parser);
 	recur_edmonds_karp(map);
+	free_map(map);
 	return (0);
 }
